3175db9a4f3fbb1ac0de126081d2253030469f41
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-03 08:08:50 -0600

    Minor updates

--- include/notification_queue.hpp
+++ include/notification_queue.hpp
@@ -4,6 +4,8 @@
 #include <condition_variable>
 #include <functional>
 
+using lock_t = std::unique_lock<std::mutex>;
+
 class notification_queue {
   
   std::deque<std::function<void()>>  _q;
@@ -14,7 +16,7 @@ class notification_queue {
 public:
 
   bool try_pop(std::function<void()>& x) {
-    std::unique_lock<std::mutex> lock{_mutex, try_to_lock};
+    lock_t lock{_mutex, std::try_to_lock};
     if (!lock || _q.empty()) return false;
     x = std::move(_q.front());
     _q.pop_front();
@@ -24,7 +26,7 @@ public:
   template <typename F>
   bool try_push(F&& f) {
     {
-      std::unique_lock<std::mutex> lock{_mutex, try_to_lock};
+      lock_t lock{_mutex, std::try_to_lock};
       if (!lock) return false;
       _q.emplace_back(std::forward<F>(f));
     }
@@ -34,14 +36,14 @@ public:
   
   void done() {
     {
-      std::unique_lock<std::mutex> lock{_mutex};
+      lock_t lock{_mutex};
       _done = true;
     }
     _ready.notify_all();
   }
   
   bool pop(std::function<void()>& x) {
-    std::unique_lock<std::mutex> lock{_mutex};
+    lock_t lock{_mutex};
     while (_q.empty()) _ready.wait(lock);
     if (_q.empty()) return false;
     x = std::move(_q.front());
@@ -52,7 +54,7 @@ public:
   template <typename F>
   void push(F&& f) {
     {
-      std::unique_lock<std::mutex> lock{_mutex};
+      lock_t lock{_mutex};
       _q.emplace_back(std::forward<F>(f));
     }
     _ready.notify_one();
